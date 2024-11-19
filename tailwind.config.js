108c6607e06bb1bf50a6ac3be8d1cf0b81d6123b
Bozorov06 <qoyilbekbozorov160@gmail.com>
2024-11-19 06:48:35 +0500

    hamma file joylandi

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        blek: "#282b22",
+        "dark-green": "#3d4a39",
+      },
+
+      fontFamily: {
+        "Afacad": "Afacad Flux",
+      },
+    },
+  },
+  plugins: [],
+};
+
+
